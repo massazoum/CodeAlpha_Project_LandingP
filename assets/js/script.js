e387b0773d4ef3d6919af751e9e6d23705262309
massa <zoumaniguimassa12@gmail.com>
2023-09-19 11:39:00 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
-const swiper_wrapper =document.getElementById('swiper-wrapper');
+const swiper_wrapper = document.getElementById('swiper-wrapper');
+
 const swiper = new Swiper('.swiper', {
- direction: 'horizontal',
- loop: true,
- slidesPerView: 3,
- spaceBetween: 20,
- navigation: {
-   nextEl: '.swiper-button-next',
-   prevEl: '.swiper-button-prev',
- },
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // When window width is less than or equal to 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // Add more breakpoints as needed
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+  },
 });
+
 
 const dataLatest = [
   {
