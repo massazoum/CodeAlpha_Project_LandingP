aeabcd2a31c976be6b81cf44942256f9fec0d081
massa <zoumaniguimassa12@gmail.com>
2023-09-19 12:50:55 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const swiper = new Swiper('.swiper', {
     },
     // Add more breakpoints as needed
     992: {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 25,
     },
   },
